a6e16f800851fc915007a79532916fe0a26351f3
Vani-1107 <vaniguptaa2003@gmail.com>
2023-09-23 02:56:46 +0530

    chatbot

---
+++ chatbot/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css'
+import config from './config';
+import MessageParser from './MessageParser';
+import ActionProvider from './ActionProvider';
+import React from 'react';
+function App() {
+  return(
+    <div className='App'>
+          <Chatbot
+            config={config}
+            messageParser={MessageParser}
+            actionProvider={ActionProvider}
+             setIsChatbotNonFunctional={setIsChatbotNonFunctional}
+          />
+    </div>
+  );
+};
+export default App;
